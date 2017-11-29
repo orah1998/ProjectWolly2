073f80b704902bd4a80cfb3f4f45421ff97ac169
yair <yairefaeli324@gmail.com>
2017-11-29 13:29:17 +0200

    i add cell test class and update board, and update cell

---
+++ Tests/playerTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by yair on 28/11/17.
+//
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../Board.h"
+#include "../Player.h"
+#include "../GameLogics.h"
+
+TEST(ClassDeclaration,CellTestValue) {
+    Board b=Board(8);
+    int sizeOfBoard=b.getSizeOfArray();
+    Cell** board = b.getArrayOfCells();
+    char symbol = 'X';
+    string name="comp";
+    GameLogics logic=GameLogics(b.getArrayOfCells(),b.getSizeOfArray());
+    logic.NextMove('X');
+    Player p = Player(b.getArrayOfCells(),'X',"bob");
+
+}
+TEST(ClassDeclaration,CellValueChange) {
+    Cell c = Cell(0,0);
+    c.ChangeValue('x');
+    EXPECT_TRUE(c.GetValue() == 'x');
+}
